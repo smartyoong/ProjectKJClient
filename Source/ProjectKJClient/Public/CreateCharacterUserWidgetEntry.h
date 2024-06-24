b1bc7dc8da4fc62109614c282f2e39aaabd2ad9c
Kim Young Chan <smartyoong@naver.com>
2024-06-25 01:18:55 +0900

    엔트리 UI 추가 이제 엔트리에 추가할 SkeletalMesh View Item Actor 만들자

---
+++ Source/ProjectKJClient/Public/CreateCharacterUserWidgetEntry.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Blueprint/IUserObjectListEntry.h"
+#include "CreateCharacterUserWidgetEntry.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTKJCLIENT_API UCreateCharacterUserWidgetEntry : public UUserWidget//, IUserObjectListEntry
+{
+	GENERATED_BODY()
+	
+};
